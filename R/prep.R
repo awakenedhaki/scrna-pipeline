fa9fb5d8a891667d46c4cbffcbbbb55864b6cea0
Rodrigo Vallejos <rodrigovallejos@Rodrigos-MacBook-Pro.local>
2021-03-25 21:57:56 -0700

    Updating documentation.

--- R/prep.R
+++ R/prep.R
@@ -5,40 +5,48 @@
 # Helpers ======================================================================
 
 # Functions ====================================================================
-
-#' @title Prepare data for processing
+#' @title Prepare data for processing.
 #'
-#' @param path10x Path to CellRanger output directory
-#' @param experiment.name String, experiment identifier
+#' @details Reading 10x sample directory containing `barcodes.tsv`, `genes.tsv` and `matrix.mtx`
+#'   and encapsulating the data as a \code{\link{SingleCellExperiment}} object.
+#'
+#'   The data is prepared for downstream processing by:
+#'   * Renaming column names to the `Barcode` IDs.
+#'   * Renaming row names in to ENSEMBL ID-gene symbol pairs.
+#'   * Adding a experiment identifier column for ease of dataset integration.
+#'   * Replacing \code{NA} values with character representation \code{"NA"}.
+#'
+#'   \code{\link{SummarizedExperiment}} is imported since \code{\link[SummarizedExperiment]{rowData}}
+#'   is not within the \code{\link{SingleCellExperiment}} namespace.
+#'
+#' @param path10x String, path to 10X sample data.
+#' @param identifier String, experiment identifier
 #'
 #' @importFrom DropletUtils read10xCounts
-#' @importFrom SummarizedExperiment rowData
+#' @importFrom SummarizedExperiment rowData colData
 #' @importFrom scater uniquifyFeatureNames
 #' @importFrom tidyr replace_na
 #' @importFrom magrittr %>%
 #'
 #' @return \code{\link{SingleCellExperiment}} object
 #' @export
-prep.data <- function(path10x, experiment.name) {
+prep.data <- function(path10x, identifier) {
+  # TODO:
+  # . Include @seealso for functions used in implementation?
   sce <- DropletUtils::read10xCounts(samples = path10x)
 
-  # Rename column names in colData to Barcode IDs
   colnames(sce) <- sce$Barcode
-
-  # Rename row names in colData to ensembl ID-gene symbol pairs
   rownames(sce) <- scater::uniquifyFeatureNames(ID = rownames(sce),
 											                          names = SummarizedExperiment::rowData(sce)$Symbol)
 
-  # Create column in colData with experiment name for downstream group_by
-  sce$experment.name <- experiment.name
+  sce$experiment.name <- identifier
 
-  # Replace NA with 'NA' in rowData column names
   colnames(SummarizedExperiment::rowData(sce)) <- sce %>%
     SummarizedExperiment::rowData() %>%
     names() %>%
     tidyr::replace_na(replace = 'NA')
 
-  save.processed(sce, 'prep.rds')
-
+  # Save data in ./data/processed/ directory
+  save.processed(sce, 'prep', identifier)
   return(sce)
 }
