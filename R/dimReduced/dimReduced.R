257bce3ca0135e5cfd393578061138c64341b64b
Rodrigo Vallejos <rodrigovallejos@Rodrigos-MacBook-Pro.local>
2021-03-28 21:39:56 -0700

    Rejoining protocol scripts with their respective pipeline dispatcher. roxygen2 cannot handle subdirectories in R/.

--- R/dimReduced/dimReduced.R
+++
@@ -1,8 +0,0 @@
-dimReduced <- function(sce, protocol, seed = 100, params) {
-  set.seed(seed)
-  if (protocol == 'vanilla') {
-    sce <- .reduced.vanilla(sce, params)
-  }
-
-  return(sce)
-}
--- R/normCounts/normVanilla.R
+++
@@ -1,9 +0,0 @@
-# Vanilla ======================================================================
-
-#' @importFrom scater librarySizeFactors logNormCounts
-.norm.vanilla <- function(sce, ...) {
-  norm.sce <- scater::librarySizeFactors(sce, ...) %>%
-    scater::logNormCounts()
-
-  return(norm.sce)
-}
