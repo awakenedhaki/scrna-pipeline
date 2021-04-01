7b943f5ad78f3a8fdb65cd2c95c32552fbc30c0b
Rodrigo Vallejos <rodrigovallejos@Rodrigos-MacBook-Pro.local>
2021-03-31 22:46:29 -0700

    Removing logging and saving functions.

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,3 @@
-# Data processing ==============================================================
-
 #' TODO: Documentation
 .kwargs <- function(f, param, kwargs) {
   do.call(purrr::partial(f, param), kwargs)
@@ -16,80 +14,24 @@
   return(which(matcher(pattern, strings)))
 }
 
-# Logging ======================================================================
+#' TODO: documentation
+.read.variance.model <- function(identifier) {
+  # TODO: Handle multiple model files.
+  diagnostic.path <- here(getwd(), 'data', 'diagnostic')
 
-#' TODO: Add documentation
-#'
-#' @param name String, name for logger
-#'
-#' @importFrom here here
-#' @importFrom futile.logger flog.logger appender.file
-generate.log <- function(name) {
-  filename <- paste0(name, '.log')
-
-  if (!file.exists(here(filename))) {
-    file.create(filename)
+  diagnostic.files <- list.files(diagnostic.path)
+  pattern <- paste0(paste('featureSelect-.*', identifier, sep = '-'), '.rds')
+  variance.model.idx <- .where('featureSelect-.*\\.rds', diagnostic.files, matcher = grepl)
+  if (length(variance.model.idx) == 0) {
+    stop('Fitted variance model data must be within diagnostic directory.')
   }
-  futile.logger::flog.logger(name, appender = futile.logger::appender.file(filename))
+
+  model.path <- here(diagnostic.path, diagnostic.files[variance.model.idx])
+  model <- readRDS(model.path)
+  return(model)
 }
 
 #' TODO: Add documentation
-#'
-#' @param msg String, output message
-#'
-#' @importFrom futile.logger flog.info
-console.logger <- function(msg) {
-  futile.logger::flog.info(msg)
-}
-
-#' TODO: Add documentation
-#'
-#' @param msg String, output message
-#'
-#' @importFrom futile.logger flog.info
-file.logger <- function(msg) {
-  futile.logger::flog.info(msg, name = 'analysis')
-}
-
-# Saving =======================================================================
-
-#' TODO: Add documentation
-#'
-#' @param obj \code{\link{SingleCellExperiment}} object
-#' @param name String, name of file
-#'
-#' @importFrom here here
-save.processed <- function(obj, name) {
-  file.name <- paste0(name, '.rds')
-  saveRDS(obj, file = here(getwd(), 'data', 'processed', file.name))
-
-  console.logger(paste('Saved', name, 'within data/processed directory as RDS.'))
-  file.logger(paste('Saved', name, 'within data/processed directory as RDS.'))
-}
-
-#' TODO: Add documentation
-#'
-#' @param obj \code{\link{SingleCellExperiment}} object
-#' @param name String, name of file
-#'
-#' @importFrom here here
-save.diagnostic <- function(obj, name) {
-  file.name <- paste0(name, '.rds')
-  saveRDS(obj, file = here(getwd(), 'data', 'diagnostic', file.name))
-
-  console.logger(paste('Saved', name, 'within data/diagnostic directory as RDS.'))
-  file.logger(paste('Saved', name, 'within data/diagnostic directory as RDS.'))
-}
-
-#' TODO: Add documentation
-#'
-#' @param obj \code{\link{SingleCellExperiment}} object
-#' @param name String, name of file
-#'
-#' @importFrom here here
-save.output <- function(obj, name) {
-  saveRDS(obj, file = here(getwd(), 'output', name))
-
-  console.logger(paste('Saved', name, 'within data/output directory as RDS.'))
-  file.logger(paste('Saved', name, 'within data/output directory as RDS.'))
+.blackbox.namespace <- function(name) {
+  base::get(name, envir = as.environment('package:blackbox'))
 }
