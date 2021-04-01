7e49b9dd892a1063ea5ce7baea3cc7be3969fa07
Rodrigo Vallejos <rodrigovallejos@Rodrigos-MacBook-Pro.local>
2021-03-31 17:03:19 -0700

    Refactoring dimReduced.R

--- R/dimReduced.R
+++ R/dimReduced.R
@@ -1,4 +1,8 @@
-dimReduced <- function(sce, protocol, seed = 100, params) {
+#' TODO: documentation
+dimReduced <- function(sce,
+                       protocol,
+                       seed = 100,
+                       params) {
   set.seed(seed)
   if (protocol == 'vanilla') {
     sce <- .reduced.vanilla(sce, params)
@@ -9,55 +13,83 @@ dimReduced <- function(sce, protocol, seed = 100, params) {
 
 # Vanilla ======================================================================
 
+#' TODO: documentation
 #' @importFrom scater runTSNE runUMAP
 .reduced.vanilla <- function(sce, params) {
-  sce <- .pca(sce, hv.genes = hv.genes, params$pca)
+  sce <- .pca(sce, params$pca)
   sce <- .kwargs(scater::runTSNE, sce, params$tsne)
   sce <- .kwargs(scater::runUMAP, sce, params$umap)
 }
 
+#' TODO: documentation
 #' @importFrom scater runPCA
 #' @importFrom PCAtools findElbowPoint
 #' @importFrom scran denoisePCA
 #' @importFrom SingleCellExperiment reducedDim
-.pca <- function(sce, hv.genes, params) {
+.pca <- function(sce, params) {
   params$scater$subset_row <- .get.hvg.subset(sce, params$subset.name)
-  sce <- .kwargs(scater::runPCA, sce, params$scater)
 
-  if ('infer' %in% names(params) & !(ncomponents %in% names(params))) {
-    if (params$infer == 'elbow') {
-      ncomponents <- findElbowPoint(.explained.variance(sce))
-    } else if (params$infer == 'denoise') {
-      variance.model <- .read.variance.model(...)
-      ncomponents <- ncol(scran::denoisePCA(sce,
-                                            technical = variance.model,
-                                            subset.row = hv.genes))
-    }
-  }
+  pca.sce <- .kwargs(scater::runPCA, sce, params$scater)
 
-  SingleCellExperiment::reducedDim(sce, 'PCA') <-
-    SingleCellExperiment::reducedDim(sce, 'PCA')[, 1:params$ncomponents]
-  return(sce)
+  ncomponents <- .get.ncomponents(sce, params)
+  SingleCellExperiment::reducedDim(pca.sce, 'PCA') <-
+    SingleCellExperiment::reducedDim(pca.sce, 'PCA')[, 1:ncomponents]
+
+  return(pca.sce)
 }
 
+#' TODO: documentation
+.get.ncomponents <- function(sce, params) {
+  if ('infer' %in% names(params) & !('ncomponents' %in% names(params))) {
+    ncomponents <- .infer.ncomponents(sce, params)
+  } else {
+    ncomponents <- pca$ncomponents
+  }
+
+  return(ncomponents)
+}
+
+#' TODO: documentation
+.infer.ncomponents <- function(sce, params) {
+  if (params$infer == 'elbow') {
+    ncomponents <- findElbowPoint(.explained.variance(sce))
+  } else if (params$infer == 'denoise') {
+    variance.model <- .read.variance.model()
+    # TODO: Finding better way of passing hv.genes logical vector to denoisePCA
+    denoised <- scran::denoisePCA(sce,
+                                  technical = variance.model,
+                                  subset.row = params$scater$subset_row)
+    ncomponents <- ncol(reducedDim(denoised))
+  }
+  return(ncomponents)
+}
+
+#' TODO: documentation
 #' @importFrom SingleCellExperiment rowSubset
-.get.hv.genes <- function(sce, subset.name) {
+.get.hvg.subset <- function(sce, subset.name) {
   SingleCellExperiment::rowSubset(sce, field = subset.name)
 }
 
+#' TODO: documentation
 .read.variance.model <- function() {
-  diagnostic.files <- list.files(here(getwd(), 'data', 'diagnostic'))
-  variance.model.idx <- .where('featureSelect-.*\\.rds', diagnostic.files, matcher = grepl)
+  # TODO: Handle multiple model files.
+  diagnostic.path <- here(getwd(), 'data', 'diagnostic')
 
+  diagnostic.files <- list.files(diagnostic.path)
+  variance.model.idx <- .where('featureSelect-.*\\.rds', diagnostic.files, matcher = grepl)
   if (length(variance.model.idx) == 0) {
     stop('Fitted variance model data must be within diagnostic directory.')
   }
 
-  model <- readRDS(diagnostic.files[variance.model.idx])
+  model.path <- here(diagnostic.path, diagnostic.files[variance.model.idx])
+  model <- readRDS(model.path)
   return(model)
 }
 
+#' TODO: documentation
 #' @importFrom SingleCellExperiment reducedDim
 .explained.variance <- function(sce) {
-  attr(SingleCellExperiment::reducedDim(sce), 'percentVar')
+  . <- attr(SingleCellExperiment::reducedDim(sce), 'percentVar')
+  save.diagnostic(., 'dimReduced-explained-variance')
+  return(.)
 }
